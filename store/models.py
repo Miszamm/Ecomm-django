a10bc3c8fc6eb1c3befd2a56a04a13e08621f550
Michal Misza <miszamm@yahoo.ie>
2023-04-02 14:41:59 +0100

    cart and store functionality

--- store/models.py
+++ store/models.py
@@ -36,12 +36,12 @@ variation_category_choice = (
 
 class Variation(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    variation_category =models.CharField(max_length=100, choices=variation_category_choice)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
     variation_value = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now=True)
 
-    object = VariationManager()
+    objects = VariationManager()
 
-    def __unicode__(self):
-        return self.product
+    def __str__(self):
+        return self.variation_value
